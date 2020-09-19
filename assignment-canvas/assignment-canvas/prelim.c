16b0995062c5fc9c4ba6756d62bc5a36b49f7683
abhinavg18 <abhinavvk@gmail.com>
2020-09-19 13:39:53 -0400

    assigment1 part1

--- assignment-canvas/assignment-canvas/prelim.c
+++ assignment-canvas/assignment-canvas/prelim.c
@@ -1,8 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
+
+void check(int x) {
+	if (x == -1) {
+		printf("ERROR, no hostname");
+	}
+}
 int main () {
+
+  char name[256];
+  int hostname;
+
+  hostname = gethostname(name, sizeof(name));
+  check(hostname);
+
+  printf("Hostname: %s\n", name);
 	
-  
-  printf("Hostname: %s\n", hostname);
+ 
   return 0;
 }
